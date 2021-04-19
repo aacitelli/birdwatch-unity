aafa3c5ee984b7193b97b276e9fe8da947f3b741
Anden Acitelli <a.acitelli@gmail.com>
2021-04-19 14:21:31 -0400

    Cull chunks too far from player

--- Assets/TerrainManager.cs
+++ Assets/TerrainManager.cs
@@ -22,6 +22,7 @@ public class TerrainManager : MonoBehaviour
     void Update() 
     {
         GenerateChunks();
+        CullChunks();
     }
 
     void GenerateChunks()
@@ -31,19 +32,34 @@ public class TerrainManager : MonoBehaviour
         int tileDepth = (int) tileSize.z;
 
         // Instantiate a tile at the given position
+        Vector2Int playerPosChunks = new Vector2Int(
+            (int) player.transform.position.x / tileWidth, 
+            (int) player.transform.position.z / tileDepth);
+
+        // Iterate through chunks around player to see if they need generated 
         int xStart, xEnd, zStart, zEnd;
-        xStart = ((int) player.transform.position.x / tileWidth) - generateRadius; 
-        xEnd = ((int) player.transform.position.x / tileWidth) + generateRadius;
+        xStart =  playerPosChunks.x - generateRadius; 
+        xEnd = playerPosChunks.x + generateRadius;
         for (int xIndex = xStart; xIndex < xEnd; xIndex++)
         {
-            zStart = ((int) player.transform.position.z / tileDepth) - generateRadius; 
-            zEnd = ((int) player.transform.position.z / tileDepth) + generateRadius;
+            zStart = playerPosChunks.y - generateRadius; 
+            zEnd = playerPosChunks.y + generateRadius;
             for (int zIndex = zStart; zIndex < zEnd; zIndex++)
             {
+                // Chunk pos (in chunks)
                 Vector2 pos = new Vector2(xIndex, zIndex);
+
+                // Generate in circular radius; 
+                // Simpler (and barely less optimal) than trying to generate iteration order through a sphere in the first place 
+                if (Vector2.Distance(pos, playerPosChunks) > generateRadius)
+                {
+                    continue;
+                }
+
+                // Only generate chunk if it doesn't already exist 
                 if (!chunks.ContainsKey(pos))
                 {
-                    // Calculate position 
+                    // Chunk pos (in absolute world coordinates) 
                     Vector3 chunkPos = new Vector3(this.gameObject.transform.position.x + xIndex * tileWidth, 
                         this.gameObject.transform.position.y, 
                         this.gameObject.transform.position.z + zIndex * tileDepth);
@@ -54,6 +70,36 @@ public class TerrainManager : MonoBehaviour
                     chunks[pos] = tile;
                 }                
             }
+        }
+    }
+
+    // Removes elements 
+    // TODO: Unload, but do not completely get rid of, chunks that are not within range 
+    void CullChunks()
+    {
+        Vector3 tileSize = tilePrefab.GetComponent<MeshRenderer>().bounds.size;
+        int tileWidth = (int) tileSize.x;
+        int tileDepth = (int) tileSize.z;
+
+        Vector2Int playerPosChunks = new Vector2Int(
+            (int) player.transform.position.x / tileWidth, 
+            (int) player.transform.position.z / tileDepth);
+
+        // Get a list of chunks to remove; we can't actually remove them yet, as you can't modify a dictionary during iterations 
+        List<Vector2> keysToRemove = new List<Vector2>();
+        foreach (KeyValuePair<Vector2, GameObject> chunkPair in chunks)
+        {
+            if (Vector2.Distance(chunkPair.Key, playerPosChunks) > generateRadius)
+            {
+                keysToRemove.Add(chunkPair.Key);
+            }
+        }
+
+        // Actually destroy the ones that were out of range 
+        foreach (Vector2 chunkKey in keysToRemove)
+        {
+            Destroy(chunks[chunkKey]);
+            chunks.Remove(chunkKey);
         }
     }
 }
