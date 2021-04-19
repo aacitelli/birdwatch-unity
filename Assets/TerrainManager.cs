3608a43a29f683481fa896aa94a639a00f348349
Anden Acitelli <a.acitelli@gmail.com>
2021-04-19 13:47:21 -0400

    Continuously generate chunks in player radius

--- Assets/TerrainManager.cs
+++ Assets/TerrainManager.cs
@@ -10,31 +10,49 @@ public class TerrainManager : MonoBehaviour
     [SerializeField]
     private GameObject tilePrefab;
 
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private GameObject player; 
+
+    [SerializeField]
+    private int generateRadius;
+
+    // Holds references to chunks we've generated so that we don't regenerate them 
+    private Dictionary<Vector2, GameObject> chunks = new Dictionary<Vector2, GameObject>();
+
+    void Update() 
     {
-        GenerateMap();
+        GenerateChunks();
     }
 
-    void GenerateMap()
+    void GenerateChunks()
     {
         Vector3 tileSize = tilePrefab.GetComponent<MeshRenderer>().bounds.size;
         int tileWidth = (int) tileSize.x;
         int tileDepth = (int) tileSize.z;
 
         // Instantiate a tile at the given position
-        for (int xIndex = 0; xIndex < mapWidthInTiles; xIndex++)
+        int xStart, xEnd, zStart, zEnd;
+        xStart = ((int) player.transform.position.x / tileWidth) - generateRadius; 
+        xEnd = ((int) player.transform.position.x / tileWidth) + generateRadius;
+        for (int xIndex = xStart; xIndex < xEnd; xIndex++)
         {
-            for (int zIndex = 0; zIndex < mapDepthInTiles; zIndex++)
+            zStart = ((int) player.transform.position.z / tileDepth) - generateRadius; 
+            zEnd = ((int) player.transform.position.z / tileDepth) + generateRadius;
+            for (int zIndex = zStart; zIndex < zEnd; zIndex++)
             {
-                // Calculate position 
-                Vector3 pos = new Vector3(this.gameObject.transform.position.x + xIndex * tileWidth, 
-                    this.gameObject.transform.position.y, 
-                    this.gameObject.transform.position.z + zIndex * tileDepth);
+                Vector2 pos = new Vector2(xIndex, zIndex);
+                if (!chunks.ContainsKey(pos))
+                {
+                    // Calculate position 
+                    Vector3 chunkPos = new Vector3(this.gameObject.transform.position.x + xIndex * tileWidth, 
+                        this.gameObject.transform.position.y, 
+                        this.gameObject.transform.position.z + zIndex * tileDepth);
 
-                // Instantiate new tile GameObject 
-                // Syntax: Instantiate(<prefab>, <parent transform>, <rotation>)
-                GameObject tile = Instantiate(tilePrefab, pos, Quaternion.identity) as GameObject;
+                    // Instantiate new tile GameObject 
+                    // Syntax: Instantiate(<prefab>, <parent transform>, <rotation>)
+                    GameObject tile = Instantiate(tilePrefab, chunkPos, Quaternion.identity) as GameObject;
+                    chunks[pos] = tile;
+                }                
             }
         }
     }
